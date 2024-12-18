f0be7e6184b73926a1c6927b2ec560fb318b1823
Emanuel Lopes Silva <162730193+EmanuelSilva69@users.noreply.github.com>
2024-12-18 12:54:42 -0300

    Update Breadth_First_Search.py
    Atualizar os python dos subprocessadores

--- Breadth_First_Search.py
+++ Breadth_First_Search.py
@@ -11,7 +11,7 @@ if not os.path.isfile('walls_data.json'): #cria o arquivo caso não exista
     print("O arquivo 'walls_data.json' não foi encontrado. Gerando agora...")
     time.sleep(2)
     # Executa o script GerarMaze.py para criar o arquivo
-    subprocess.run(["E:/projetos python/.venv/Scripts/python.exe", "GerarMaze.py"])
+    subprocess.run(["python", "GerarMaze.py"])
     print('\035[31m' + "Reinicie o Main! Estava criando arquivos necessários." + '\035[0m')
 
 # carrega o arquivo
